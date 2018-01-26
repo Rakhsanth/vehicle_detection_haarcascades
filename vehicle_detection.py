e53e29bed8152019b4778913821bdec72f6c939b
Rakhsanth R <35163960+Rakhsanth@users.noreply.github.com>
2018-01-26 21:49:33 +0530

    Update vehicle_detection.py

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -3,26 +3,27 @@
 import cv2
 print(cv2.__version__)
 
+#Trained haar cascade file..
 cascade_src = 'cars.xml'
 video_src = 'dataset/video1.avi'
 #video_src = 'dataset/video2.avi'
-
+#For capturing from video file
 cap = cv2.VideoCapture(video_src)
 car_cascade = cv2.CascadeClassifier(cascade_src)
 
 while True:
-    ret, img = cap.read()
-    if (type(img) == type(None)):
+    ret, frame = cap.read()
+    if (type(frame) == type(None)):
         break
     
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     cars = car_cascade.detectMultiScale(gray, 1.1, 1)
 
     for (x,y,w,h) in cars:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)      
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)      
     
-    cv2.imshow('video', img)
+    cv2.imshow('video', frame)
     
     if cv2.waitKey(33) == 27:
         break
